dd02a5f68e6c991598ce08ce0315aa2ea17316ac
Dulce Mercado <soydulceangelina@gmail.com>
2020-11-15 12:14:31 -0500

    details section finished

--- src/components/productDetails/ProductDetails.jsx
+++ src/components/productDetails/ProductDetails.jsx
@@ -5,6 +5,7 @@ import { productDetailsSelector } from '../../store/products/productsSelectors';
 import { setSelectedProductQuantity } from '../../store/products/productsActions';
 import { addToShoppingCart } from '../../store/shoppingCart/shoppingCartActions';
 import styles from './ProductDetails.module.css';
+import { toDollarCurrency } from '../../utils';
 
 export const ProductDetails = () => {
   const selectedProduct = useSelector(productDetailsSelector);
@@ -23,38 +24,54 @@ export const ProductDetails = () => {
     <div className={styles.cart}>
       <h4 className={styles.title}>Product</h4>
       <div className={styles.container}>
-        <div>
-          count
-          {selectedProductQuantity}
-        </div>
         {selectedProduct && (
-          <ProductItem
-            src={selectedProduct.img}
-            alt="productDetail"
-          />
+          <>
+            <div className={styles.count}>
+              {selectedProductQuantity}
+            </div>
+            <ProductItem
+              src={selectedProduct.img}
+              alt="productDetail"
+            />
+            <div className={styles.handle_container}>
+              <div>
+                {selectedProduct.category}
+                {' '}
+                •
+                {' '}
+                <span className={styles.currency}>
+                  {toDollarCurrency(selectedProduct.price)}
+                </span>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  onClick={handleQuantityClick(selectedProductQuantity - 1)}
+                  disabled={selectedProductQuantity === 1}
+                  className={styles.handle_subtraction}
+                >
+                  -
+                </button>
+                <button
+                  type="button"
+                  onClick={handleQuantityClick(selectedProductQuantity + 1)}
+                  className={styles.handle_sum}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <p className={styles.description}>{selectedProduct.description}</p>
+            <button
+              type="button"
+              onClick={handleAddToShoppingCartClick}
+              className={styles.addToCart}
+            >
+              add to cart
+            </button>
+          </>
         )}
       </div>
-      <div>
-        <button
-          type="button"
-          onClick={handleQuantityClick(selectedProductQuantity - 1)}
-          disabled={selectedProductQuantity === 1}
-        >
-          -
-        </button>
-        <button
-          type="button"
-          onClick={handleQuantityClick(selectedProductQuantity + 1)}
-        >
-          +
-        </button>
-      </div>
-      <button
-        type="button"
-        onClick={handleAddToShoppingCartClick}
-      >
-        add to cart
-      </button>
     </div>
   );
 };
